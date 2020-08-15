e12c55bf429037810ad6deeddce6edc887b0b243
ali sdg <5607006-alisdg9@users.noreply.gitlab.com>
2020-08-15 17:20:17 +0430

    AlgMapper Output

--- src/main/java/ir/ac/sbu/reducer/AlgorithmReducer.java
+++ src/main/java/ir/ac/sbu/reducer/AlgorithmReducer.java
@@ -11,24 +11,27 @@ import java.util.List;
 public class AlgorithmReducer extends Reducer<Text, Node, Text, Node> {
     @Override
     protected void reduce(Text key, Iterable<Node> values, Context context) throws IOException, InterruptedException {
-        double dampingFactor = context.getConfiguration().getDouble("damp", 0.85);
-        double contributions = 0.0;
-        double pageRank;
-        Node primary = new Node(true);
+//        double dampingFactor = context.getConfiguration().getDouble("damp", 0.85);
+//        double contributions = 0.0;
+//        double pageRank;
+//        Node primary = new Node(true);
+//
+//        for (Node val : values) {
+//            if(val.isProcessing()) {
+//                primary = val ;
+//                continue;
+//            }
+//            contributions += val.getRank();
+//            context.write(key,val);
+//        }
+//
+//        //Compute new page rank
+//        pageRank = (1.0 - dampingFactor) + (dampingFactor * contributions);
+//        primary.setRank(pageRank)
+//        context.write(key, primary);
 
         for (Node val : values) {
-            if(val.isProcessing()) {
-                primary = val ;
-                continue;
-            }
-            contributions += val.getRank();
             context.write(key,val);
         }
-
-        //Compute new page rank
-        pageRank = (1.0 - dampingFactor) + (dampingFactor * contributions);
-        primary.setRank(pageRank);
-
-        context.write(key, primary);
     }
 }
